186c919c4b8e805589d51f5d97a569b901c1a8d7
danadim <92433648+YordankaKristina@users.noreply.github.com>
2023-06-23 18:46:09 +0300

    Fix typo

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ const Home = () => (
       <span className='blue_gradient text-center'> Prompts</span>
     </h1>
     <p className='desc text-center'>
-      Beetopia is an open-source prompting tool for modern world to
+      Beetopia is a prompting tool for modern world to
       discover, create and share creative prompts.
     </p>
 
